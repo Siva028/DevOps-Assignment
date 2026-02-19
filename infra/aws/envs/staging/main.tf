05ca81821c5244b7ec87ad7814816d4cbbca77a9
Siva <siva.subramanian3@icloud.com>
2026-02-19 18:27:22 +0530

    Module ALB was commented out earlier so I have removed it and added back

--- infra/aws/envs/staging/main.tf
+++ infra/aws/envs/staging/main.tf
@@ -1,14 +1,12 @@
 module "vpc" {
   source = "../../modules/vpc"
 }
-/*
 module "alb" {
   source = "../../modules/alb"
 
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
 }
-*/
 module "ecr" {
   source = "../../modules/ecr"
 }
